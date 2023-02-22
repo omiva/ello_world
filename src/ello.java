492cb3c2861d8d54935dec07367b51a0543e276e
Yuvaraj M <yuvm19is@cmrit.ac.in>
2023-02-22 19:08:28 +0530

    Initial commit

---
+++ src/ello.java
@@ -0,0 +1,9 @@
+public class ello
+{
+    public static void main(String[] args)
+    {
+        long TheFirstNumberEver= 8012971927989877l;
+
+        System.out.println(TheFirstNumberEver);
+    }
+}
